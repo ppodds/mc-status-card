8a566cef33b68c1a5331851f1f4364760dd542ec
ppodds <oscar20020629@gmail.com>
2022-07-07 00:53:34 +0800

    Fix: INPUT env

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
+touch .env
+for INPUT in $(echo $INPUTS | jq -r 'to_entries|map("INPUT_\(.key|ascii_upcase)=\(.value|@uri)")|.[]'); do
+    echo $INPUT >> .env
+done
+
 docker pull ppodds/mc-status-card:latest
-docker run ppodds/mc-status-card
+docker run --env-file .env ppodds/mc-status-card
