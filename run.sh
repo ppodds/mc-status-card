73a0d412a3b251bb596ec732d4891fc35d4777d3
ppodds <oscar20020629@gmail.com>
2022-07-07 17:11:58 +0800

    Fix: miss github env

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ touch .env
 for INPUT in $(echo $INPUTS | jq -r 'to_entries|map("INPUT_\(.key|ascii_upcase)=\(.value|@uri)")|.[]'); do
     echo $INPUT >> .env
 done
+env | grep -E '^(GITHUB|ACTIONS|CI|TZ)' >> .env
 
 docker pull ppodds/mc-status-card:latest
 docker run --env-file .env ppodds/mc-status-card
